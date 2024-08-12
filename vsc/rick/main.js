bce95858e7a1c9e48396ced8e0c653578f01a4c9
MatiZur <166444039+MatiZur@users.noreply.github.com>
2024-08-12 17:51:52 -0300

    Cambiada página trinítica
    Eso pancho, te resumo lo que hice: corregí el bug de que cuando buscabas, ponías la última página y borrabas el input no te dejaba avanzar a la siguiente pagina (eso llevo a otro bug que si la última página de lo buscado era 1 también, te dejaba. Eso también está solucionado). También agregue que cuando el h1 con los números de las páginas eran de un sólo dígito te pone un 0 antes para hacer que el input, botones, etc. no se mueva (o casi) cada vez que pasa del 9 al 10 o viceversa.

---
+++ vsc/rick/main.js
@@ -0,0 +1,73 @@
+
+let filterInput = document.querySelector('input');
+let btnPrev = document.querySelector('#prev');
+let btnNext = document.querySelector('#next');
+btnPrev.addEventListener('click', handlePrev);
+btnNext.addEventListener('click', handleNext);
+filterInput.addEventListener('input', handleInput);
+filterInput.value = '';
+
+
+let currentPage = 1;
+let currentName = null;
+var lastPage = 42;
+
+function handlePrev() {
+  if (currentPage > 1) currentPage--;
+  if (currentPage < lastPage) btnNext.disabled = false;
+  if (currentPage == 1) btnPrev.disabled = true;
+  fetchCharacters(currentPage, currentName);
+}
+
+function handleNext() {
+  if (currentPage < lastPage) currentPage++;
+  if (currentPage > 1) btnPrev.disabled = false;
+  if (currentPage == lastPage) btnNext.disabled = true;
+  fetchCharacters(currentPage, currentName);
+}
+
+function fetchCharacters(page, name) {
+  let url = 'https://rickandmortyapi.com/api/character';
+  url += `?page=${page ? page : 1}`;
+  if (name) url += `&name=${name}`;
+  console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      lastPage = data.info.pages;
+      if(page>=1 && page <=9 && lastPage>=1 && lastPage <=9) document.querySelector('#page').textContent = `0${page}/0${lastPage}`;
+      else if(page>=1 && page <=9 && lastPage>9) document.querySelector('#page').textContent = `0${page}/${lastPage}`;
+      else if(page>9 && lastPage>=1 && lastPage<=9) document.querySelector('#page').textContent = `${page}/0${lastPage}`;
+      else if(page>9 && lastPage>9) document.querySelector('#page').textContent = `${page}/${lastPage}`;
+      let container = document.querySelector('.container');
+      container.innerHTML = '';
+      for (let character of data.results) {
+        let card = document.createElement('div');
+        let title = document.createElement('h1');
+        let img = document.createElement('img');
+        card.classList.add('card');
+        title.textContent = character.name;
+        img.src = character.image;
+        card.append(img);
+        card.append(title);
+        container.append(card);
+        if (currentPage == lastPage) btnNext.disabled = true;
+      }
+    });
+}
+
+function handleInput(e) {
+  if (e.target.value.length >= 1 && e.target.value.length <= 3) return;
+  if (e.target.value == '') {
+    currentPage = 1;
+    currentName = null;
+  } else {
+    currentName = e.target.value;
+    currentPage = 1;
+  }
+  btnPrev.disabled = true;
+  btnNext.disabled = false;
+  fetchCharacters(currentPage, currentName);
+}
+
+fetchCharacters(currentPage, currentName);
